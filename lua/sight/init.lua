6a3ea7890373613bf7cd06efcb90355bdefd8432
r4ppz <johnreyrabosa.f@gmail.com>
2026-04-25 21:22:05 +0800

    refactor(init): simplify floating window configuration

--- lua/sight/init.lua
+++ lua/sight/init.lua
@@ -2,28 +2,34 @@ require("sight.types")
 
 local M = {}
 
----@param buf number
----@param opts FloatOpts
----@return number
+local default_config = {
+  width = 60,
+  height = 40,
+  border = "rounded",
+}
+
+---@param buf? number
+---@param opts? FloatOpts
+---@return number win_id
 function M.create_floating_window(buf, opts)
   buf = buf or 0
-  opts = opts or {}
+  opts = vim.tbl_deep_extend("force", default_config, opts or {})
+
+  if buf == 0 then
+    buf = vim.api.nvim_get_current_buf()
+  end
 
   if not vim.api.nvim_buf_is_valid(buf) then
     error("Invalid buffer")
   end
 
-  local width = opts.width or 30
-  local height = opts.height or 30
-  local border = opts.border or "single"
-
   local win_config = {
     relative = "cursor",
     row = 1,
     col = 0,
-    width = width,
-    height = height,
-    border = border,
+    width = opts.width,
+    height = opts.height,
+    border = opts.border,
   }
 
   vim.keymap.set("n", "q", "<cmd>close<cr>", {
