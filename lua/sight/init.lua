ec4f9e30d7baad112f280e03cfcce3069c06b6f1
r4ppz <johnreyrabosa.f@gmail.com>
2026-04-27 17:58:16 +0800

    refactor(init): remove types for now and improve opts merging

--- lua/sight/init.lua
+++ lua/sight/init.lua
@@ -2,26 +2,30 @@ require("sight.types")
 
 local M = {}
 
----@type FloatOpts
-local default_win_config = {
+local default_opts = {
   width = 70,
   height = 15,
-  row = 1,
-  col = 1,
   border = "single",
   enter = true,
 }
 
----@param buf? number
----@param opts? FloatOpts
----@return number win_id
+M.config = {}
+
+M.setup = function(user_opts)
+  user_opts = user_opts or {}
+
+  for key, _ in pairs(user_opts) do
+    if default_opts[key] == nil then
+      error(string.format("Invalid configuration key: %s", key))
+    end
+  end
+
+  M.config = vim.tbl_extend("force", default_opts, user_opts)
+end
+
 function M.create_floating_window(buf, opts)
   buf = buf or 0
-  opts = vim.tbl_deep_extend("force", default_win_config, opts or {})
-
-  if buf == 0 then
-    buf = vim.api.nvim_get_current_buf()
-  end
+  opts = opts or {}
 
   if not vim.api.nvim_buf_is_valid(buf) then
     error("Invalid buffer")
@@ -29,11 +33,11 @@ function M.create_floating_window(buf, opts)
 
   local win_config = {
     relative = "cursor",
-    row = opts.row,
-    col = opts.col,
     width = opts.width,
     height = opts.height,
     border = opts.border,
+    row = 1,
+    col = 1,
   }
 
   vim.keymap.set("n", "q", "<cmd>close<cr>", {
@@ -48,10 +52,6 @@ function M.create_floating_window(buf, opts)
 
   local win = vim.api.nvim_open_win(buf, opts.enter, win_config)
   return win
-end
-
-M.setup = function()
-  vim.print("Hello World")
 end
 
 return M
--- lua/sight/types.lua
+++
@@ -1,9 +0,0 @@
----@alias FloatBorder "none" | "single" | "double" | "rounded"
-
----@class FloatOpts
----@field width? number The width of the window (default: 40)
----@field height? number The height of the window (default: 10)
----@field row? number The row position relative to cursor (default: 1)
----@field col? number The column position relative to cursor (default: 1)
----@field border? FloatBorder The border style (default: "rounded")
----@field enter? boolean Whether to move cursor into the window (default: true)
