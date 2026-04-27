b3ded2f4a075277713190c32b51e4dabb0230962
r4ppz <johnreyrabosa.f@gmail.com>
2026-04-27 18:16:38 +0800

    fix: improve the merging problem

--- lua/sight/init.lua
+++ lua/sight/init.lua
@@ -1,31 +1,29 @@
-require("sight.types")
-
 local M = {}
 
 local default_opts = {
-  width = 70,
-  height = 15,
-  border = "single",
+  window = {
+    width = 70,
+    height = 15,
+    border = "single",
+  },
+
   enter = true,
+
+  keymaps = {
+    close = "q",
+  },
 }
 
 M.config = {}
 
 M.setup = function(user_opts)
-  user_opts = user_opts or {}
-
-  for key, _ in pairs(user_opts) do
-    if default_opts[key] == nil then
-      error(string.format("Invalid configuration key: %s", key))
-    end
-  end
-
-  M.config = vim.tbl_extend("force", default_opts, user_opts)
+  M.config = vim.tbl_deep_extend("force", default_opts, user_opts or {})
 end
 
-function M.create_floating_window(buf, opts)
-  buf = buf or 0
-  opts = opts or {}
+function M.create_floating_window(buf, local_opts)
+  buf = buf or vim.api.nvim_get_current_buf()
+
+  local config = vim.tbl_deep_extend("force", M.config, local_opts or {})
 
   if not vim.api.nvim_buf_is_valid(buf) then
     error("Invalid buffer")
@@ -33,24 +31,31 @@ function M.create_floating_window(buf, opts)
 
   local win_config = {
     relative = "cursor",
-    width = opts.width,
-    height = opts.height,
-    border = opts.border,
     row = 1,
     col = 1,
+    width = config.window.width,
+    height = config.window.height,
+    border = config.window.border,
   }
 
-  vim.keymap.set("n", "q", "<cmd>close<cr>", {
+  vim.keymap.set("n", config.keymaps.close, "<cmd>close<cr>", {
     buffer = buf,
     silent = true,
+    nowait = true,
     desc = "Close Sight Window",
   })
 
-  vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
-  vim.api.nvim_set_option_value("readonly", true, { buf = buf })
-  vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf })
+  local buf_options = {
+    modifiable = false,
+    readonly = true,
+    bufhidden = true,
+  }
 
-  local win = vim.api.nvim_open_win(buf, opts.enter, win_config)
+  for opt, val in ipairs(buf_options) do
+    vim.api.nvim_set_option_value(opt, val, { buf = buf })
+  end
+
+  local win = vim.api.nvim_open_win(buf, config.enter, win_config)
   return win
 end
 
